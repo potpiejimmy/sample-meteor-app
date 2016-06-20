61ef5d1fe786e6b04533ec48bba3850ce48f9654
Thorsten <thorsten@potpiejimmy.de>
2016-06-20 22:25:31 +0200

    sample crud one pf datable

--- client/app.ts
+++ client/app.ts
@@ -7,21 +7,44 @@ import { Mongo }     from 'meteor/mongo';
 import { DataTable } from 'primeng/primeng';
 import { Column } from 'primeng/primeng';
 import { MeteorComponent } from 'angular2-meteor';
+import { Button } from 'primeng/primeng';
+import { Dialog } from 'primeng/primeng';
+import { InputText } from 'primeng/primeng';
+
 
 @Component({
   selector: 'app',
-  directives: [DataTable,Column],
+  directives: [DataTable,Column,Button,Dialog,InputText],
   templateUrl: 'client/app.html'
 })
 class App extends MeteorComponent { 
   parties: Array<Object>;
+  currentParty: Object;
+  displayDialog: boolean;
   
   constructor() {
     super();
-	this.autorun(() => {
-		this.parties = Parties.find().fetch();
-	}, true);
+    this.displayDialog = false;
+    this.autorun(() => {
+      this.parties = Parties.find().fetch();
+    }, true);
   }
+
+  onRowSelect(event) {
+      this.currentParty = event.data;
+      this.displayDialog = true;
+  }
+
+  save() {
+      this.currentParty = null;
+      this.displayDialog = false;
+  }
+  
+  delete() {
+      this.currentParty = null;
+      this.displayDialog = false;
+  }    
+  
 }
  
 bootstrap(App);
